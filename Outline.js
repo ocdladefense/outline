ad3e9eb30c90e9a8f1f94658c2fc89266545053e
SullivanKE <KatelynSullivan135@gmail.com>
2025-01-16 13:30:06 -0800

    Cleanup of comments and addition of updated documentation.

--- Outline.js
+++ Outline.js
@@ -1,6 +1,6 @@
 import OutlineEntry from "./OutlineEntry.js";
-export default class Outline {
 
+export default class Outline {
   static DEFAULTS = {
     selectors: ["h1", "h2", "h3", "h4", "h5", "h6"],
     itemClassName: "outline-item",
@@ -12,42 +12,39 @@ export default class Outline {
   // The flat array of items in the outline.
   #items;
 
+  /**
+   * Creates a new outline from the given props.
+   * @param {Object} [props={}] - Configuration options for the outline.
+   * @param {Array<string>} [props.selectors=["h1", "h2", "h3", "h4", "h5", "h6"]] - CSS selectors for elements to include in the outline. These can be any valid CSS selector. The order given determins the hierarchy of the outline.
+   * @param {string} [props.itemClassName="outline-item"] - CSS class name to apply to each outline item.
+   * @param {string} [props.rootClassName="outline-content"] - CSS class name to apply to the root element of the outline.
+   * @example
+   * const outline = new Outline({
+   *   selectors: ["h1", "h2", "h3"],
+   *   itemClassName: "my-outline-item",
+   *   rootClassName: "my-outline-content"
+   * });
+   */
   constructor(props) {
     this.#config = {...Outline.DEFAULTS, ...props};
     this.#items = [];
-    //this.handleIntersection = this.handleIntersection.bind(this);
   }
 
-  // /**
-  //  * Returns the flat array of items in the outline in order of appearance.
-  //  * @returns {Array<OutlineEntry>} Flat array of all outline items in order.
-  //  */
-  // getFlattened() {
-  //   return this.#items;
-  // }
-
-  // /**
-  //  * Nests children based on their level into other outline items.
-  //  * @returns {Array<OutlineItems>} Nested array of top level outline items with order of appearance preserved.
-  //  */
-  // getNested() {
-  //   return Outline.nestChildren(this.#items);
-  // }
-
-  // /**
-  //  * Creates an outline from the current document.
-  //  * @returns {Outline}
-  //  */
-  // static fromCurrentDocument() {
-  //   //TODO: Each outline item contains a text label, id, and level.
-  //   // Outline class takes in document from which the outline will be built.
-  //   return new Outline(document);
-  // }
 
   /**
-   * Searches the instanced document for the outline items based on the selectors passed in and maintains the hierarchy based on the order of arguments passed in.
-   * @param {...String} selectors List of selectors to search for in order of importance.
+   * Builds an outline from the provided DOM node using the configured selectors.
+   * 
+   * @param {Node} node - The DOM node to build the outline from.
+   * @param {boolean} [flattened=false] - Whether to return a flat list of items or a nested structure.
+   * @returns {Array|Object} The outline items as a flat array if `flattened` is true, otherwise as a nested structure.
+   * 
+   * @example
+   * const outline = new Outline();
+   * const node = document.querySelector('.content');
+   * const items = outline.build(node, true); // Returns a flat list of outline items.
+   * const nestedItems = outline.build(node); // Returns a nested outline structure.
    */
+
   build(node, flattened=false) {
     let selectors = this.#config.selectors;
 
@@ -177,42 +174,6 @@ export default class Outline {
       if (outline.children.length > 0)
         root.appendChild(recNestLists(outline.children));
     });
-
-    return root;
-  }
-
-  toNodeTree(callback) {
-    let outlines = this.getNested(this.#items);
-
-    // Create our root list node and add some styling
-    const root = document.createElement("ul");
-    root.setAttribute("class", this.#config.rootClassName);
-
-    // This is our recursive function to nest the lists
-    // It takes the children array of an outline and wraps it in an unordered list while checking for grandchildren.
-    // It also converts the children to list items and adds them to the parent list.
-    let recNestLists = (outlines, parent) => {
-      const list = document.createElement("ul");
-      list.setAttribute("class", this.#config.itemClassName);
-
-      // Go through each child
-      outlines.map((outline) => {
-        // Convert it to a list item and add it to the list
-        list.appendChild(outline.toListItem());
-
-        // If it has children, perform this same function on the child array and append that list to the current one.
-        if (outline.children.length > 0) recNestLists(outline.children, list);
-
-        // Call the callback function
-        callback(outline, list);
-      });
-
-      // Append the list to the parent
-      parent.appendChild(list);
-    };
-
-    // Create our top level items in our list.
-    recNestLists(outlines, root);
 
     return root;
   }
